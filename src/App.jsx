d8896be7be7c5c1fc840a40b3834386ec4f9bccd
amanmishra75way <amishra.75way@gmail.com>
2025-06-24 11:43:04 +0530

    Add Toggle component and integrate render props in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import React from "react";
 import WithAuth from "./utils/WithAuth";
 import Dashboard from "./components/Dashboard";
+import Toggle from "./components/Toggle";
 
 const App = () => {
   const data = {
     name: "Aman Mishra",
     age: 25,
   };
+  // Using Higher order component
   const AuthDashboard = WithAuth(Dashboard);
   return (
-    <div>
-      <AuthDashboard data={data} />
-    </div>
+    <>
+      <div>
+        <AuthDashboard data={data} />
+      </div>
+
+      {/* Using Render Props */}
+      <Toggle
+        render={() => <h1>This is a render prop modified from outside</h1>}
+      />
+
+      <Toggle
+        render={() => {
+          return (
+            <div>
+              <h1>This is another render prop modified from outside</h1>
+              <p>It can be anything, even a component!</p>
+            </div>
+          );
+        }}
+      />
+    </>
   );
 };
 
